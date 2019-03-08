bca2b164999a528b55bf5932787ae6661edce74d
swoogle <cocoonlee@gmail.com>
2019-03-08 16:58:05 +0800

    修改词频统计代码

--- wordcount/wordcount.py
+++ wordcount/wordcount.py
@@ -5,8 +5,8 @@ from collections import Counter
 
 # wordcount 给定一篇英文文档（input.txt），统计其中单词词频，将结果输入到文件中（output.txt)
 
-fi = open('input.txt')
-words = fi.read().lower()
+with open('input.txt') as fi:
+	words = fi.read().lower()
 fi.close()
 	
 pattern = re.compile(r'[a-zA-Z]+\-?[a-zA-Z]*') #判断是否为英文单词
@@ -15,3 +15,4 @@ word_list = Counter(pattern.findall(words))
 with open('output.txt', 'w') as fo:
 	for word in word_list.most_common():
 		fo.write("%s -> %d\n" % word)
+fo.close()
