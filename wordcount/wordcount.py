939a7be3a648e5258a7bbf68fb3c731cd2fea064
swoogle <cocoonlee@gmail.com>
2019-04-03 12:42:10 +0800

    修改词频统计代码

--- wordcount/wordcount.py
+++ wordcount/wordcount.py
@@ -1,18 +1,20 @@
 #! /usr/bin/env python3
 
 import re
+import sys
+import os.path as op
 from collections import Counter
 
 # wordcount 给定一篇英文文档（input.txt），统计其中单词词频，将结果输入到文件中（output.txt)
 
-with open('input.txt') as fi:
+with open(op.join(sys.path[0], 'input.txt')) as fi:
 	words = fi.read().lower()
 fi.close()
 	
 pattern = re.compile(r'[a-zA-Z]+\-?[a-zA-Z]*') #判断是否为英文单词
 word_list = Counter(pattern.findall(words))
 
-with open('output.txt', 'w') as fo:
+with open(op.join(sys.path[0], 'output.txt'), 'w') as fo:
 	for word in word_list.most_common():
 		fo.write("%s -> %d\n" % word)
 fo.close()
