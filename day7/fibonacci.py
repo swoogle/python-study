391cd229458d995b2330164e013230554ea43aae
swoogle <cocoonlee@gmail.com>
2019-05-20 22:24:57 +0800

    更新函数学习代码

---
+++ day7/fibonacci.py
@@ -0,0 +1,13 @@
+import os
+import time
+
+
+def main():
+    f = [1 , 1]
+    for i in range(2, 20):
+        f += [f[i - 1] + f[i - 2]]
+    for val in f:
+        print(val, end=' ')
+
+if __name__ == '__main__':
+    main()
