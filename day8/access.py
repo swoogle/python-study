3857d216fbacf77d98c3ba7348626e9dece96f89
swoogle <cocoonlee@gmail.com>
2019-05-25 22:16:17 +0800

    更新类学习代码

---
+++ day8/access.py
@@ -0,0 +1,21 @@
+#! /usr/bin/env python3
+#! _*_ coding: utf-8 _*_
+
+class Test:
+
+    def __init__(self, foo):
+        self.__foo = foo
+
+    def __bar(self):
+        print(self.__foo)
+        print('__bar')
+
+
+def main():
+    test = Test('hello')
+    test._Test__bar()
+    print(test._Test__foo)
+
+
+if __name__ == "__main__":
+    main()
