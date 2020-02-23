b31b4310cda0ff692a59bbf42dbde9297e68eb84
swoogle <cocoonlee@gmail.com>
2020-02-23 16:29:19 +0800

    添加matplotlib学习代码

---
+++ day9/mpl_squares.py
@@ -0,0 +1,18 @@
+#! /usr/bin/env python3
+#! _*_ coding: utf-8 _*_
+
+import matplotlib.pyplot as plt
+
+input_values = [1, 2, 3, 4, 5]
+squares = [1, 4, 9, 16, 25]
+plt.plot(input_values, squares, linewidth=5)
+
+# 设置图表标题、坐标轴标签
+plt.title("Square Numbers", fontsize=24)
+plt.xlabel("Value", fontsize=14)
+plt.ylabel("Square of Value", fontsize=14)
+
+# 设置刻度标记的大小
+plt.tick_params(axis='both', labelsize=14)
+
+plt.show()
