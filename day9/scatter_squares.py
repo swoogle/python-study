db7ce1d105bf0e89e5a08b99e4ee123335851ebf
swoogle <cocoonlee@gmail.com>
2020-02-23 22:23:59 +0800

    添加matplotlib学习代码

--- day9/scatter_squares.py
+++ day9/scatter_squares.py
@@ -6,6 +6,9 @@ import matplotlib.pyplot as plt
 x_values = list(range(1, 1001))
 y_values = [x**2 for x in x_values]
 
+# 设置绘图窗口大小
+plt.figure(figsize=(10, 6))
+
 plt.scatter(x_values, y_values, c=y_values,
             cmap=plt.cm.Blues, edgecolor='none', s=40)
 
