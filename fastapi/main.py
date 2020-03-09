9eec231d23e04fe1341c4cd6593e1599d3ff9afd
swoogle <cocoonlee@gmail.com>
2020-03-09 12:04:21 +0800

    fastAPI学习

---
+++ fastapi/main.py
@@ -0,0 +1,14 @@
+#! /usr/bin/env python3
+#! _*_ coding: utf-8 _*_
+
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+def read_root():
+    return {"Hello": "fastAPI"}
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: str = None):
+    return {"item_id": item_id, "q": q}
